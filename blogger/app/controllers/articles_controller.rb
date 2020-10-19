049f3db935100e1cb2dfa16f2c495212a1460738
Meron Ogbai <okbaymeron@gmail.com>
2020-10-19 17:39:47 +0300

    Add delete, edit and update

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -18,4 +18,20 @@ class ArticlesController < ApplicationController
     @article.save
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+  
+    redirect_to article_path(@article)
+  end
 end
